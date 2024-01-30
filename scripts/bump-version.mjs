b5976f2faa0a833a2cb8fe0eeffc0faad6f3b3fc
pcmagas <pcmagas@disroot.org>
2024-01-30 21:29:06 +0000

    Bump Version

--- scripts/bump-version.mjs
+++ scripts/bump-version.mjs
@@ -2,28 +2,56 @@ import inquirer  from 'inquirer';
 import * as fs from 'fs';
 import semver from 'semver';
 
+
+async function getVersionBumpChoices(currentVersion) {
+
+  const versionChoices = ['patch', 'minor', 'major'];
+
+  const messages = {
+    'patch': "Patch (Bug fixes)",
+    'minor': "Minor (New features)",
+    'major': "Major (Breaking changes)" 
+  }
+
+  // Generate choice objects with custom messages based on the current version
+  const choices = versionChoices.map((bumpType) => {
+
+    const bumpedVersion = semver.inc(currentVersion, bumpType);
+    
+    return {
+      name: `${messages[bumpType]}  -> ${bumpedVersion}`,
+      value: bumpedVersion,
+    };
+
+  });
+
+  return choices;
+}
+
 async function bumpVersion() {
   // Read package.json
   const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
+  console.log(`Current version ${packageJson.version}`)
+
+  const choices = await getVersionBumpChoices(packageJson.version);
   // Ask the user for the new version
   const { versionType } = await inquirer.prompt([
     {
       type: 'list',
       name: 'versionType',
       message: 'Select the version bump type:',
-      choices: ['patch', 'minor', 'major'],
+      choices: choices,
     },
   ]);
 
   // Bump the version
-  const newVersion = semver.inc(packageJson.version, versionType);
-  packageJson.version = newVersion;
+  packageJson.version = versionType;
 
   // Write the updated package.json
   fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
 
-  console.log(`Version bumped to ${newVersion}`);
+  console.log(`Version bumped to ${versionType}`);
 }
 
 bumpVersion();
